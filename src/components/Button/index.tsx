4fcddb94ab0f80e333ef743b25812fd2fc721882
JerryLiao-GoSky <jerryliao@goskyai.com>
2022-01-04 14:49:33 +0800

    fix: button variants too heavy

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,12 +7,12 @@ import {
   position,
   space,
   typography,
-  variant,
 } from 'styled-system';
 import { MainColorType } from '../../assets/theme/global';
 import { CommonButtonProps } from '../../assets/types/ElementTypes';
 import { getColorKey } from '../../utils/styled/color';
 import { exceptionList } from '../../utils/styled/exceptionList';
+import { buttonSizeVariant, buttonStyleTypeVariant } from './buttonVariant';
 
 export type ButtonSizeType = 'small' | 'normal';
 export type ButtonStyleType = 'primary' | 'secondary';
@@ -71,56 +71,8 @@ const StyledButton = styled.button.withConfig({
     border-color: ${({ theme }) => theme.colors[getColorKey('grey', '200')]};
   }
 
-  ${({ themeColor }) =>
-    variant({
-      prop: 'styleType',
-      variants: {
-        primary: {
-          color: 'white',
-          backgroundColor: themeColor,
-          borderColor: themeColor,
-          '&:hover': {
-            backgroundColor: getColorKey(themeColor, '300'),
-          },
-          '&:active': {
-            backgroundColor: getColorKey(themeColor, '400'),
-            borderColor: getColorKey(themeColor, '400'),
-          },
-          '&:disabled': {
-            backgroundColor: getColorKey('grey', '200'),
-          },
-        },
-        secondary: {
-          backgroundColor: 'white',
-          color: getColorKey(themeColor, '400'),
-          borderColor: getColorKey(themeColor, '400'),
-          '&:hover': {
-            color: getColorKey(themeColor, '300'),
-          },
-          '&:active': {
-            color: themeColor,
-            borderColor: themeColor,
-          },
-          '&:disabled': {
-            color: getColorKey('grey', '300'),
-          },
-        },
-      },
-    })}
-  ${variant({
-    prop: 'size',
-    variants: {
-      normal: {
-        px: '1.25rem',
-        py: '0.5rem',
-      },
-      small: {
-        px: '1rem',
-        py: '0.5rem',
-        fontSize: '0.75rem',
-      },
-    },
-  })}
+  ${({ themeColor }) => buttonStyleTypeVariant(themeColor)}
+  ${buttonSizeVariant()}
   ${compose(border, layout, position, space, typography)}
 `;
 /* eslint-enable indent */
