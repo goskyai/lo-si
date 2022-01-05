dae6a712e2cf15aaaa002fb98d1775e079be7966
JerryLiao-GoSky <jerryliao@goskyai.com>
2022-01-05 10:54:53 +0800

    fix: style of Text

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,13 +1,3 @@
-import { Typography, TypographyProps } from 'antd';
-import styled from 'styled-components';
+import { Typography } from 'antd';
 
-const { Text: AntdText } = Typography;
-
-export type TextProps = Pick<TypographyProps, 'Text'>['Text'];
-
-const StyledText = styled(AntdText)<TextProps>`
-  line-height: 1.3;
-  font-family: ${({ theme }) => theme.fontFamily.default};
-`;
-
-export const Text = StyledText;
+export const Text = Typography.Text;
