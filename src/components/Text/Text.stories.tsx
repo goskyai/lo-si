f691ea83c224dd10621726d31f594490c6b9e0cc
JerryLiao-GoSky <jerryliao@goskyai.com>
2022-01-05 10:38:30 +0800

    fix: style of text

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,18 +1,51 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-
-import { Text } from '.';
+import { Text, TextProps } from '.';
 
 export default {
   title: 'Components/Text',
   component: Text,
-  parameters: {
-    layout: 'centered',
-  },
-} as ComponentMeta<typeof Text>;
+  argTypes: {},
+} as ComponentMeta<TextProps>;
 
-const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
+const Template: ComponentStory<TextProps> = (args) => (
+  <Text {...args}>Hello World!</Text>
+);
 
 export const Default = Template.bind({});
-Default.args = {
-  children: 'HELLO WORLD!',
-};
+Default.args = {};
+
+export const Secondary = Template.bind({});
+Secondary.args = { type: 'secondary' };
+
+export const Success = Template.bind({});
+Success.args = { type: 'success' };
+
+export const Warning = Template.bind({});
+Warning.args = { type: 'warning' };
+
+export const Danger = Template.bind({});
+Danger.args = { type: 'danger' };
+
+export const Disabled = Template.bind({});
+Disabled.args = { disabled: true };
+
+export const Mark = Template.bind({});
+Mark.args = { mark: true };
+
+export const Code = Template.bind({});
+Code.args = { code: true };
+
+export const Keyboard = Template.bind({});
+Keyboard.args = { keyboard: true };
+
+export const Underline = Template.bind({});
+Underline.args = { underline: true };
+
+export const Delete = Template.bind({});
+Delete.args = { delete: true };
+
+export const Strong = Template.bind({});
+Strong.args = { strong: true };
+
+export const Italic = Template.bind({});
+Italic.args = { italic: true };
--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,49 +1,13 @@
-import * as CSS from 'csstype';
-import { FunctionComponent } from 'react';
+import { Typography, TypographyProps } from 'antd';
 import styled from 'styled-components';
-import {
-  border,
-  BorderProps,
-  compose,
-  layout,
-  LayoutProps,
-  position,
-  PositionProps,
-  space,
-  SpaceProps,
-  textStyle,
-  TextStyleProps,
-  typography,
-  TypographyProps,
-} from 'styled-system';
-import { color, ColorProps } from '../../utils/styled/color';
-import { exceptionList } from '../../utils/styled/exceptionList';
 
-export interface TextProps
-  extends SpaceProps,
-    ColorProps,
-    TypographyProps,
-    LayoutProps,
-    BorderProps,
-    PositionProps,
-    TextStyleProps {
-  as?: string | React.ComponentType<Record<string, unknown>>;
-  htmlFor?: string;
-  whiteSpace?: CSS.Property.WhiteSpace;
-}
+const { Text: AntdText } = Typography;
 
-const StyledText = styled.p.withConfig<TextProps>({
-  shouldForwardProp: (prop, validator) =>
-    !exceptionList.includes(prop) && validator(prop),
-})`
-  line-height: 1.4;
-  font-size: ${({ theme }) => theme.fontSizes.h4};
-  margin: 0;
-  display: block;
-  white-space: ${({ whiteSpace }) => whiteSpace || 'pre-line'};
-  ${compose(border, layout, position, space, typography, color, textStyle)}
+export type TextProps = Pick<TypographyProps, 'Text'>['Text'];
+
+const StyledText = styled(AntdText)<TextProps>`
+  line-height: 1.3;
+  font-family: ${({ theme }) => theme.fontFamily.default};
 `;
 
-export const Text: FunctionComponent<TextProps> = ({ children, ...props }) => {
-  return <StyledText {...props}>{children}</StyledText>;
-};
+export const Text = StyledText;
