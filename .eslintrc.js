28eb740237bd5b9fcc81cb32b3cb345c081baee7
leo-gosky <leolin@goskyai.com>
2022-01-05 18:41:18 +0800

    build(storybook): upgrade to 6.4.9

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,18 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:storybook/recommended"],
   rules: {
     "no-console": "warn",
-    indent: ["warn", 2, { SwitchCase: 1 }],
+    indent: ["warn", 2, {
+      SwitchCase: 1
+    }],
     quotes: ["warn", "single"],
     semi: ["warn", "always"],
     "comma-dangle": ["warn", "always-multiline"],
     "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-empty-interface": [
-      "error",
-      {
-        allowSingleExtends: true,
-      },
-    ],
-  },
+    "@typescript-eslint/no-empty-interface": ["error", {
+      allowSingleExtends: true
+    }]
+  }
 };
