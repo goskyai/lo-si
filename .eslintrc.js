87f28431352f29a9922e11d302400e5cc6cffed5
leo-gosky <leolin@goskyai.com>
2022-01-06 16:12:51 +0800

    chore: update eslint sequence

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,31 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:storybook/recommended"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+    'prettier',
+  ],
   rules: {
-    "no-console": "warn",
-    indent: ["warn", 2, {
-      SwitchCase: 1
-    }],
-    quotes: ["warn", "single"],
-    semi: ["warn", "always"],
-    "comma-dangle": ["warn", "always-multiline"],
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-empty-interface": ["error", {
-      allowSingleExtends: true
-    }]
-  }
+    'no-console': 'warn',
+    indent: [
+      'warn',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'always'],
+    'comma-dangle': ['warn', 'always-multiline'],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-empty-interface': [
+      'error',
+      {
+        allowSingleExtends: true,
+      },
+    ],
+  },
 };
