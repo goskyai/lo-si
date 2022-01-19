66200ee292eacbea9c1402363d5ae17da22571d0
leo-gosky <leolin@goskyai.com>
2022-01-19 11:51:07 +0800

    refactor: convert to antd breadcrumb

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -1,47 +1,48 @@
-import { To } from 'history';
-import _ from 'lodash';
-import { FunctionComponent } from 'react';
 import {
-  Link as RouterLink,
-  LinkProps as RouterLinkProps,
-} from 'react-router-dom';
+  Breadcrumb as ABreadcrumb,
+  BreadcrumbInterface,
+  BreadcrumbProps,
+} from 'antd';
 import styled from 'styled-components';
 import { getColorValue } from '../../utils/styled/color';
-import { Text } from '../Text';
+import { Icon } from '../Icon';
 
-const StyledLink = styled(RouterLink)<RouterLinkProps>`
-  text-decoration: none;
-  color: ${getColorValue('grey')};
-  transition: color 0.2s ease;
-  &:hover {
-    color: ${getColorValue('grey-500')};
+export const Breadcrumb = styled<BreadcrumbInterface>(
+  ABreadcrumb,
+)<BreadcrumbProps>`
+  .ant-breadcrumb-separator {
+    display: inline-flex;
+    color: ${getColorValue('grey', '300')};
+    .anticon {
+      font-size: 0.5rem;
+    }
+  }
+
+  &.ant-breadcrumb {
+    .ant-breadcrumb-link {
+      color: ${getColorValue('grey', '500')} !important;
+
+      &:hover {
+        color: ${getColorValue('grey', '700')} !important;
+      }
+
+      * {
+        color: inherit;
+      }
+    }
+
+    > span:last-child {
+      .ant-breadcrumb-link {
+        color: ${getColorValue('gosky-blue')} !important;
+
+        * {
+          color: inherit;
+        }
+      }
+    }
   }
 `;
 
-type LinkType = {
-  to: To;
-  label: string;
-};
-type LastItemType = {
-  label: string;
-};
-export type BreadcrumbItemsType = [...LinkType[], LastItemType];
-export interface BreadcrumbProps {
-  items: BreadcrumbItemsType;
-}
-
-export const Breadcrumb: FunctionComponent<BreadcrumbProps> = ({ items }) => {
-  const links = _.slice(items, 0, items.length - 1) as LinkType[];
-  const lastItem = _.last(items) as LastItemType;
-  return (
-    <Text>
-      {links.map((link, index) => (
-        <span key={index}>
-          <StyledLink to={link.to}>{link.label}</StyledLink>
-          {' > '}
-        </span>
-      ))}
-      {lastItem.label}
-    </Text>
-  );
+Breadcrumb.defaultProps = {
+  separator: <Icon icon="chevron-right" />,
 };
