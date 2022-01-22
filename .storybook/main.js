56137baabe5dff5570f8708ba83a56609f982b45
leo-gosky <leolin@goskyai.com>
2022-01-22 15:20:01 +0800

    build: install svg babel loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,4 +6,21 @@ module.exports = {
     '../src/components/**/*.stories.tsx',
   ],
   addons: ['storybook-addon-designs', '@storybook/addon-essentials'],
+  // https://github.com/storybookjs/storybook/issues/6188#issuecomment-822924831
+  babel: async (options) => {
+    options.plugins.push([
+      'babel-plugin-inline-react-svg',
+      {
+        svgo: {
+          plugins: [
+            {
+              name: 'removeViewBox',
+              active: false,
+            },
+          ],
+        },
+      },
+    ]);
+    return options;
+  },
 };
